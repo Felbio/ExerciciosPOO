4be97ba85ddaa2e5ac6ef3031df2cd3b46132f19
Felbio <felipe.j.o.86@gmail.com>
2021-09-08 21:32:28 -0300

    Exercícios de Programação Orientada a Objetos

---
+++ Exe1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Exe1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a tua idade: ");
+            double idade = int.Parse(Console.ReadLine());
+            double batida = idade * 365.25 * 24 * 60 *60;
+            Console.WriteLine("O coração em " + idade + " anos, irá bater " + batida + " de vezes.");
+            
+        }
+    }
+}
